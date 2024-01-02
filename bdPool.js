169045c1b530ae492d7cc7c67999245bd21de26f
Mr-Jmail <gamelelkazzaz@gmail.com>
2024-01-02 22:30:09 +0300

    Update bdPool.js

--- bdPool.js
+++ bdPool.js
@@ -3,7 +3,7 @@ const mysql = require("mysql")
 module.exports = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
-    password: "Jmail09022006@",
+    password: "xx@",
     database: "training",
     connectionLimit: 7
 })
